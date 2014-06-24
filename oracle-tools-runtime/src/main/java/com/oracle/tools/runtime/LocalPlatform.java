7f8a239904d8c2ad7f89b76b4c2e63893baac8d6
Brian Oliver <brian.oliver@oracle.com>
2014-06-24 14:25:15 -0400

    ORACLETOOLS-61: Introduced the concept of a Platform

---
+++ oracle-tools-runtime/src/main/java/com/oracle/tools/runtime/LocalPlatform.java
@@ -0,0 +1,54 @@
+/*
+ * File: LocalPlatform.java
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * The contents of this file are subject to the terms and conditions of 
+ * the Common Development and Distribution License 1.0 (the "License").
+ *
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License by consulting the LICENSE.txt file
+ * distributed with this file, or by consulting https://oss.oracle.com/licenses/CDDL
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file LICENSE.txt.
+ *
+ * MODIFICATIONS:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ */
+
+package com.oracle.tools.runtime;
+
+import com.oracle.tools.runtime.java.JavaApplication;
+import com.oracle.tools.runtime.java.LocalJavaApplicationBuilder;
+
+/**
+ * The {@link Platform} in which this Java Virtual Machine is running.
+ * <p>
+ * Copyright (c) 2014. All Rights Reserved. Oracle Corporation.<br>
+ * Oracle is a registered trademark of Oracle Corporation and/or its affiliates.
+ *
+ * @author Jonathan Knight
+ * @author Brian Oliver
+ */
+public class LocalPlatform implements Platform
+{
+    @Override
+    public <A extends Application, B extends ApplicationBuilder<A>> B getApplicationBuilder(Class<A> applicationClass)
+    {
+        if (JavaApplication.class.isAssignableFrom(applicationClass))
+        {
+            return (B) new LocalJavaApplicationBuilder();
+        }
+        else
+        {
+            return (B) new SimpleApplicationBuilder();
+        }
+    }
+}
---
+++ oracle-tools-runtime/src/main/java/com/oracle/tools/runtime/Platform.java
@@ -0,0 +1,55 @@
+/*
+ * File: Platform.java
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * The contents of this file are subject to the terms and conditions of 
+ * the Common Development and Distribution License 1.0 (the "License").
+ *
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License by consulting the LICENSE.txt file
+ * distributed with this file, or by consulting https://oss.oracle.com/licenses/CDDL
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file LICENSE.txt.
+ *
+ * MODIFICATIONS:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ */
+
+package com.oracle.tools.runtime;
+
+/**
+ * Provides a means to represent a platform at runtime, a server, machine or
+ * operating system on which {@link Application}s may be running, managed or deployed.
+ * <p>
+ * Copyright (c) 2014. All Rights Reserved. Oracle Corporation.<br>
+ * Oracle is a registered trademark of Oracle Corporation and/or its affiliates.
+ *
+ * @author Jonathan Knight
+ * @author Brian Oliver
+ */
+public interface Platform
+{
+    /**
+     * Obtains a suitable {@link ApplicationBuilder} for a specific class of {@link Application}.
+     *
+     * @param <A>  the type of {@link Application}
+     * @param <B>  the type of {@link ApplicationBuilder}
+     *
+     * @param applicationClass  the {@link Class} of {@link Application} for which a {@link ApplicationBuilder}
+     *                          is required
+     * @return  the {@link ApplicationBuilder}
+     *
+     * @throws IllegalArgumentException  if the {@link Platform} can't provide a suitable
+     *                                   {@link ApplicationBuilder} for the specified {@link Application}
+     *                                   {@link Class}
+     */
+    public <A extends Application, B extends ApplicationBuilder<A>> B getApplicationBuilder(Class<A> applicationClass);
+}
