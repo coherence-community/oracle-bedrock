23675d22023411323b7db8b5a4985f1c66f0412e
Jonathan Knight <jk@thegridman.com>
2022-09-14 17:33:49 +0300

    Delete mvn-gpg.sh

--- tools/bin/mvn-gpg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ $(gpg --list-keys F44DF2DB332C52EF87557C990B5F2FF38F52623D 2>/dev/null | wc -l) -eq 0 ]; then
-  gpg --quiet --batch --yes --decrypt --passphrase="${GPG_PASSPHRASE}" --output ./.mvn/maven-gpg-private.key ./.mvn/maven-gpg-private.key.gpg
-  gpg --batch --import ./.mvn/maven-gpg-private.key
-  rm ./.mvn/maven-gpg-private.key
-fi
