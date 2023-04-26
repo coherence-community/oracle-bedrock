7d052d088cbe399c2092cf80b4ae341f12550186
Jonathan Knight <jk@thegridman.com>
2023-04-26 12:08:03 +0300

    Fix the "Deploy Snapshots" action to only run for snapshot versions

---
+++ etc/deploy-snapshots.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+CURRENT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.4.0:evaluate -Dexpression=project.version -nsu | grep -e '^[[:digit:]]')
+
+if [ "${CURRENT_VERSION}" = "" ]; then
+  echo "Could not find current version from Maven"
+  exit 1
+fi
+
+if [ -z $(echo "${CURRENT_VERSION}" | grep SNAPSHOT) ]; then
+  echo "This job only deploys SNAPSHOT versions, skipping version ${CURRENT_VERSION}"
+  exit 0
+fi
+
+echo "Building version ${CURRENT_VERSION}"
+mvn -s ./.mvn/settings.xml -B clean deploy -DskipTests -Pcoherence-ce -Dgithub.build=true
